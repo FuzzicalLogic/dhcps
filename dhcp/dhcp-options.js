dd1b087aa9fd2e928bbd9167fa4fae09e6bdea5b
Donald Atkinson <fuzzicallogic@outlook.com>
2016-02-27 13:44:04 -0700

    FEATURE: preliminary support for ipaddress options
    Changed 'value' to 'code' for less cognitive dissonance

--- dhcp/dhcp-options.js
+++ dhcp/dhcp-options.js
@@ -7,7 +7,7 @@ module.exports = (MessageClass, OptionClass) => {
 	items.forEach((item) => {
 		var option = new Option(
 			item.key,
-			item.value,
+			item.code,
 			item.size,
 			item.read,
 			item.write,
@@ -28,17 +28,25 @@ module.exports = (MessageClass, OptionClass) => {
 var assert = require('assert');
 var options = {};
 var items = [
-	{ value: 1, key: 'subnetMask', type: 'ipaddress', size: 4},
-	{ value: 2, key: 'timeOffset', type: 'uint', size: 4 },
-	{ value: 12, key: 'hostName', type: 'string', size: 0 },
-	{ value: 15, key: 'domainName', type: 'string', size: 0 },
-	{ value: 50, key: 'requestedIpAddress', type: 'ipaddress', size: 4},
-	{ value: 51, key: 'ipAddressLeaseTime', type: 'uint', size: 4 },
-	{ value: 52, key: 'optionOverload', type: 'uint', size: 1 },
+	{ code: 1, key: 'subnetMask', type: 'ipaddress', size: 1 },
+	{ code: 2, key: 'timeOffset', type: 'uint', size: 4 },
+	{ code: 3, key: 'routerOption', type: 'ipaddress', size: 0 },
+	{ code: 4, key: 'timeServerOption', type: 'ipaddress', size: 0 },
+	{ code: 5, key: 'nameServers', type: 'ipaddress', size: 0 },
+	{ code: 6, key: 'domainNameServerOption', type: 'ipaddress', size: 0 },
+	{ code: 7, key: 'logServerOption', type: 'ipaddress', size: 0 },
+	{ code: 8, key: 'cookieServerOption', type: 'ipaddress', size: 0 },
+	{ code: 9, key: 'LPRServerOption', type: 'ipaddress', size: 0 },
+	{ code: 10, key: 'impressServerOption', type: 'ipaddress', size: 0 },
+	{ code: 11, key: 'resourceLocationServerOption', type: 'ipaddress', size: 0 },
+	{ code: 12, key: 'hostName', type: 'string', size: 0 },
+	{ code: 15, key: 'domainName', type: 'string', size: 0 },
+	{ code: 16, key: 'swapServerOption', type: 'ipaddress', size: 1 },
+	{ code: 50, key: 'requestedIpAddress', type: 'ipaddress', size: 1 },
+	{ code: 51, key: 'ipAddressLeaseTime', type: 'uint', size: 4 },
+	{ code: 52, key: 'optionOverload', type: 'uint', size: 1 },
 	{
-		value: 53,
-		key: 'dhcpMessageType',
-		size: 1,
+		code: 53, key: 'dhcpMessageType', size: 1,
 		read: function(buffer, offset) {
 			assert.strictEqual(buffer.readUInt8(offset++), 1);
 			return {
@@ -50,12 +58,12 @@ var items = [
 
 		}
 	},
-	{ value: 54, key: 'serverIdentifier', type: 'ipaddress', size: 4},
-	{ value: 57, key: 'maximumMessageSize', type: 'uint', size: 2 },
-	{ value: 58, key: 'renewalTimeValue', type: 'uint', size: 4	},
-	{ value: 59, key: 'rebindingTimeValue', type: 'uint', size: 4 },
-	{ value: 60, key: 'vendorClassIdentifier', type: 'string', size: 0 },
-	//{ value: 81, key: 'fullyQualifiedDomainName', type: 'string', size: 0 }
-	{ value: 118, key: 'subnetSelection', type: 'ipaddress', size: 4}
+	{ code: 54, key: 'serverIdentifier', type: 'ipaddress', size: 4},
+	{ code: 57, key: 'maximumMessageSize', type: 'uint', size: 2 },
+	{ code: 58, key: 'renewalTimeValue', type: 'uint', size: 4	},
+	{ code: 59, key: 'rebindingTimeValue', type: 'uint', size: 4 },
+	{ code: 60, key: 'vendorClassIdentifier', type: 'string', size: 0 },
+	//{ code: 81, key: 'fullyQualifiedDomainName', type: 'string', size: 0 }
+	{ code: 118, key: 'subnetSelection', type: 'ipaddress', size: 4}
 
 ];
